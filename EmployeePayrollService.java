a7ea847cce15e9495afdb24fd44b809a69e596e8
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-04-01 08:33:19 +0530

    Added:Making payroll service database as singletone

---
+++ EmployeePayrollService.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.JDBC;
+
+
+	import java.util.List;
+	public class EmployeePayrollService {
+		 public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+
+		    private List<EmployeePayrollData> employeePayrollList;
+		    private static EmployeePayrollDBService employeePayrollDBService;
+
+		    public EmployeePayrollService() {
+		        employeePayrollDBService = EmployeePayrollDBService.getInstance();
+		    }
+
+		    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList){
+		        this();
+		        this.employeePayrollList = employeePayrollList;
+		    }
+		    public List<EmployeePayrollData> readEmployeePayrollData(IOService ioservice) {
+		        if (ioservice.equals(IOService.DB_IO)) {
+		            this.employeePayrollList = employeePayrollDBService.readData();
+		        }
+		        return this.employeePayrollList;
+		    }
+
+		    public void updateEmployeeSalary(String name, double salary) {
+		        int result = employeePayrollDBService.updateEmployeeData(name, salary);
+		        if (result == 0) return;
+		        EmployeePayrollData employeePayrollData = this.getEmployeePayrollData(name);
+		        if (employeePayrollData != null) employeePayrollData.salary = salary;
+		    }
+
+		    public EmployeePayrollData getEmployeePayrollData(String name) {
+		        return this.employeePayrollList.stream()
+		                .filter(employeePayrollData -> employeePayrollData.name.equals(name))
+		                .findFirst()
+		                .orElse(null);
+		    }
+
+		    public boolean checkEmployeeInSyncWithDB(String name) {
+		        List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.getEmployeePayrollData(name);
+		        return employeePayrollDataList.get(0).equals(getEmployeePayrollData(name));
+		    }
+	}
